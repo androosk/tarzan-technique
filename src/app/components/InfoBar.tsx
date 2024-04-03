17317103957873e16f178d3d3da4b7f85865b876
androosk <atirpok3@gmail.com>
2024-04-03 13:04:28 -0600

    more changes

--- src/app/components/InfoBar.tsx
+++ src/app/components/InfoBar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function InfoBar() {
   return (
-    <Link href="#">
+    <Link href="https://calendly.com/tarzantechnique/new-client-intake-5-dollars-60-mins-brt-goals">
       <div className="bg-red-900 min-h-20 flex justify-center items-center text-white font-sans font-extralight tracking-wide uppercase sm:text-3xl">
         <h1 className="text-center">
           click <span className="text-yellow-400 font-normal">here</span> now to
