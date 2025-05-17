998dad61cd6dfac89e10ab0190d41cfec13d3d51
androosk <atirpok3@gmail.com>
2025-05-17 15:59:01 -0600

    changes to footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,14 +4,32 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Footer() {
   return (
-    <div>
-      <div
-        className={`${inter.className} py-6 bg-black flex items-center text-white uppercase w-[100vw] grid-cols-3 text-center`}
-      >
-        <p className="text-sm w-full">2024 tarzan technique</p>
-        <p>|</p>
-        <p className="text-sm w-full">site by third andrew creative</p>
+    <footer
+      className={`${inter.className} py-6 bg-black text-white uppercase w-[100vw] text-center`}
+    >
+      <div className="flex justify-center flex-col md:flex-row">
+        <p className="px-4">
+          &copy; {new Date().getFullYear()} The Tarzan Technique
+        </p>
+        <p>
+          <a
+            href="https://calendly.com/tarzantechnique/new-client-intake-5-dollars-60-mins-brt-goals"
+            className="text-blue-500 hover:text-blue-700"
+          >
+            Book A Call
+          </a>
+        </p>
       </div>
-    </div>
+      <p className="pt-3 pb-5 md:pb-0 flex flex-col md:flex-row justify-center">
+        Website Created by&nbsp;
+        <a
+          href="https://www.thirdandrewcreative.com"
+          target="_blank"
+          className="text-blue-500 hover:text-blue-700"
+        >
+          Third Andrew Creative Agency
+        </a>
+      </p>
+    </footer>
   );
 }
