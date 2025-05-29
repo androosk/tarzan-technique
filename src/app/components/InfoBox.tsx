ef206f9308216afaa6b9f5a132cc80e171e077ac
androosk <atirpok3@gmail.com>
2025-05-29 13:01:59 -0600

    removing video file and style fix

--- src/app/components/InfoBox.tsx
+++ src/app/components/InfoBox.tsx
@@ -3,7 +3,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function InfoBox() {
   return (
     <div
-      className={`${inter.className} flex flex-col py-10 px-6 w-screen items-center"`}
+      className={`${inter.className} flex flex-col mt-4 py-10 px-6 w-screen items-center bg-slate-300`}
     >
       <div className="w-auto md:w-2/3 text-red-900 sm:pb-10 font-light italic">
         <h1 className={`text-red-900 text-4xl md:leading-loose mx-auto`}>
@@ -12,7 +12,7 @@ export default function InfoBox() {
         <p className="text-end text-2xl">~ No One Ever</p>
       </div>
       <div className="w-auto text-xl font-light">
-        <p className="py-4 md:py-0">
+        <p className="py-4 md:py-2">
           You can do the traditional thing and go to couple&apos;s counseling,
           but that takes years, costs thousands of dollars and is ineffective at
           the thing you need to do most ... rescue your failing relationship.
